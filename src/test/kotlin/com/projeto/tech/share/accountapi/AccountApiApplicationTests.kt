e717bcbcd43a0745c3c419437504bb70fe7ef9e3
marcelo garcia palma <marcelo.garcia@mercadolivre.com>
2022-09-15 17:48:25 -0300

    feat: start project tech share

---
+++ src/test/kotlin/com/projeto/tech/share/accountapi/AccountApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projeto.tech.share.accountapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

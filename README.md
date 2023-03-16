f22b2330756c4a343ddd7be13dc65c9ce797cd07
marcelo garcia palma <marcelo.garcia@mercadolivre.com>
2023-03-16 18:06:12 -0300

    feat: add implementation at JwtService

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Start container from image mongoDB
 
 docker run --name account-api -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=example -p 27017:27017 mongo
 
+mongodb://root:example@localhost:27017/?ssl=false
--- src/main/kotlin/com/projeto/tech/share/accountapi/application/security/token/JwtService.kt
+++ src/main/kotlin/com/projeto/tech/share/accountapi/application/security/token/JwtService.kt
@@ -1,19 +1,38 @@
 package com.projeto.tech.share.accountapi.application.security.token
 
+import com.projeto.tech.share.accountapi.resources.repository.service.UserService
+import io.jsonwebtoken.JwtBuilder
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import java.util.*
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 
 @Component
-class JwtService(
-    val secrety: String
-) {
+class JwtService{
+    private val expiration: Long = 1000 * 15 * 60
 
-    fun generateToken(): String {
-        // como recuperar o user e criar o token
+    @Value("\${jwt.secret}")
+    private lateinit var jwtSecret: String
+
+    fun generateToken(authentication: Authentication, roles: Array<String>): String {
+        val loggedUser = authentication.principal as UserService
+        return Jwts.builder()
+            .setSubject(loggedUser.userEntity.id)
+            .setIssuer("Api account-api")
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + expiration))
+            .claim("role", roles)
+            .signWith(SignatureAlgorithm.HS512, jwtSecret.toByteArray())
+            .compact()
     }
 
-    fun isValid(jwt: String) {
-        //
-
+    fun isValid(jwt: String) = try {
+        Jwts.parser().setSigningKey(jwtSecret.toByteArray()).parseClaimsJwt(jwt);
+    } catch (ex: RuntimeException) {
+        //TODO logar erros de validação de jwt
+        false
     }
 
     fun decodeAccessToken() {
